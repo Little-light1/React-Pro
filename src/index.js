95d29cee1601842e951419d202a31ad583092e19
zhangzhen <zhen.zhang@wanbangauto.com>
2022-04-19 17:23:26 +0800

    redux接入

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import BasicRouter from "./mainRouter";
 
-ReactDOM.render(<BasicRouter></BasicRouter>, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <BasicRouter></BasicRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
